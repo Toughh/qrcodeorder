cef5b6394ff92b833bec472558755e55647e13bc
Avijit Sannigrahi <avijit.sannigrahi@j2.com>
2026-09-02 07:46:59 +0400

    ZeroThirtyNine commit

--- config/config.js
+++ config/config.js
@@ -1,2 +1,2 @@
-const N8N_BASE_URL = "https://merakya.app.n8n.cloud/webhook";
-const WEB_URL = "https://qrcodeorder038.vercel.app";
+const N8N_BASE_URL = "https://ioweyou.app.n8n.cloud/webhook";
+const WEB_URL = "https://qrcodeorder039.vercel.app";
